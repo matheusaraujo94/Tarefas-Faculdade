6b2e7f3e4c60b8cd744c07c57efe47824a761f59
matheusaraujo94 <157986764+matheusaraujo94@users.noreply.github.com>
2024-09-09 19:26:49 -0300

    Delete Senhor_dos_aneis.py

--- Senhor_dos_aneis.py
+++
@@ -1,7 +0,0 @@
-idade_maioridade = 33
-idade_atual = int(input("Digite sua idade: "))
-
-anos_para_maioridade = max(0, idade_maioridade - idade_atual)
-
-mensagem = "Faltam {} anos para você atingir a maioridade e ter a sua grande festa.".format(anos_para_maioridade) * (anos_para_maioridade > 0) + "A celebração já pode começar!" * (anos_para_maioridade == 0)
-print(mensagem)
